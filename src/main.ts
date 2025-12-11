d76a5ecbcb9a366ff256f5100061dbca83c4d634
Saroj Maharjan <sarojm@maxamation.com>
2025-12-11 18:12:28 +1100

    Allow POST method in order to create in db

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL,
-    methods: 'GET,HEAD',
+    methods: 'GET,HEAD,POST',
   });
 
   const port = process.env.PORT || 3000;
